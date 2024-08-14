b63be81b555c59a27f9cf717ca0afb3c2e881062
Harif Narippatta <harifnarippatta@gmail.com>
2024-08-14 14:18:33 +0300

    basic config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+export default App;
